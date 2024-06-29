081051b14bd9ebb9d14550feadb28acfa2ff42d9
gautham1024 <perigautamanudeep123@gmail.com>
2024-06-29 19:41:00 +0530

    pushed code

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import registerRoutes from './routes/register.js';
 import loginRoutes from './routes/login.js';
 import indexRoutes from './routes/index.js';
 import homeRoutes from './routes/home.js';
-import rssRoutes from './routes/rss.js';  // Import RSS routes
+import rssRoutes from './routes/rss.js';  
 import { connectDB } from './config/db.js';
 import http from 'http';
 import { initializeSocket } from './controllers/rssController.js';
@@ -13,23 +13,21 @@ const app = express();
 const server = http.createServer(app);
 const port = 3000;
 
-// Middleware
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Serve static files
+
 app.use(express.static('public'));
 
-// Routes
+
 app.use('/register', registerRoutes);
 app.use('/login', loginRoutes);
 app.use('/', indexRoutes);
 app.use('/home', homeRoutes);
-app.use('/rss', rssRoutes);  // Use RSS routes
+app.use('/rss', rssRoutes);  
 
-// Start server
 server.listen(port, async () => {
-    // Connect to the database
     try {
         await connectDB();
         console.log(`Database connected successfully`);
@@ -39,5 +37,5 @@ server.listen(port, async () => {
     }
 });
 
-// Initialize socket for RSS feed
+
 initializeSocket(server);
--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,9 +1,11 @@
+// loginController.js
+
 import { db } from '../config/db.js';
 
 export const login = (req, res) => {
     const { phone, password } = req.body;
 
-    const sql = 'SELECT * FROM register WHERE phone = ? AND password = ?';
+    const sql = 'SELECT fullname FROM register WHERE phone = ? AND password = ?';
     const values = [phone, password];
 
     db.execute(sql, values, (err, results) => {
@@ -14,7 +16,9 @@ export const login = (req, res) => {
         }
 
         if (results.length > 0) {
-            res.redirect('/home');
+            const { fullname } = results[0]; // Assuming 'fullname' is the field in your database
+            // Redirect to home.html with fullname as query parameter
+            res.redirect(`/home.html?fullname=${fullname}`);
         } else {
             res.status(401).send('Invalid credentials');
         }
